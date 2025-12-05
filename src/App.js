5848e213c02eb491ab22117e7b9600afe476bad2
Sree Teja <msreeteja24>
2025-12-05 23:46:29 +0530

    Router redirection using Navigate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import { BooksProvider } from "./BooksContext";
 import Books from "./components/Books";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
@@ -9,6 +14,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Books></Books>}></Route>
+          <Route path="/books" element={<Navigate to="/"></Navigate>}></Route>
         </Routes>
       </Router>
     </BooksProvider>
