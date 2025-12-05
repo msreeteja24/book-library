9fd512906e9d01ec7d8c98c4a8312a8c34b57de4
Sree Teja <msreeteja24>
2025-12-06 00:24:18 +0530

    Reading query parameters and modifying the search - useLocation

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,26 @@
 import React, { useContext } from "react";
 import { BooksContext } from "../BooksContext";
+import { useLocation } from "react-router-dom";
+//useLocation gives us access to(returns the) the current URL including all the parameters
 
 export default function Books() {
+  //we create a new object of URLSearchParams to read the query of the current URL
+  const query = new URLSearchParams(useLocation().search);
+  //Here we get the parameter from the query and assign it to a variable.
+  //the search in the bracket is the one we use for search in the query parameter.
+  const search = query.get("search") || "";
+
   const books = useContext(BooksContext);
+
+  const filteredBooks = books.filter((b) =>
+    b.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div>
       <h1> All Books </h1>
       <ul>
-        {books.map((book) => (
+        {filteredBooks.map((book) => (
           <li key={book.id}>
             {book.title} by {book.author}
           </li>
